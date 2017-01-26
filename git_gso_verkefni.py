31612ff0949da9bc35d7c9d835ff2795b76fad5f
arnarolafs <arnarol255@nemi.tskoli.is>
2017-01-26 14:07:08 +0000

    3 línur

--- git_gso_verkefni.py
+++ git_gso_verkefni.py
@@ -3,7 +3,12 @@
 #26.01.17
 
 text = open("texti.txt","w")
-text.write("halló heimur")
+text.write("halló heimur ")
+text.close()
+text = open("texti.txt", "a")
+text.write("Þetta er lína 1 ")
+text.write("þetta er lína 2 ")
+text.write("þetta er lína 3 ")
 text.close()
 
 
